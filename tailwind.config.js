90ab6bb768dd4ed6399b6da41093df15fa8eea4d
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2024-01-02 15:58:56 -0300

    ✨ feat(OffScreen): created the page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,10 @@ export default {
     extend: {
       boxShadow: {
         button: '0 7.2px 35.2px 0 #0000002e',
+        'button-light': '#ddd5d559 0 5px 80px;',
+      },
+      backgroundImage: {
+        'off-screen': 'url(/dark-bg.png)',
       },
     },
   },

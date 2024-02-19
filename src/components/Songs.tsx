d0af5a9844cec7bd95243e9fb3c71e9482624ade
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2024-02-19 15:29:18 -0300

    feat: add basic funcionality to get top songs from Spotify API

--- src/components/Songs.tsx
+++ src/components/Songs.tsx
@@ -1,5 +1,34 @@
 import { Application } from './Application';
 
+const token = localStorage.getItem('token');
+
+async function fetchWebApi(endpoint: string, method: string) {
+  const res = await fetch(`https://api.spotify.com/${endpoint}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    method,
+  });
+  return await res.json();
+}
+
+async function getTopTracks() {
+  return (
+    await fetchWebApi('v1/me/top/tracks?time_range=long_term&limit=5', 'GET')
+  ).items;
+}
+
+const songs = (await getTopTracks()) as {
+  name: string;
+  artists: Array<object>;
+}[];
+console.log(
+  songs?.map(
+    ({ name, artists }) =>
+      `${name} by ${artists.map(artist => artist.name).join(', ')}`
+  )
+);
+
 export function Songs() {
   return (
     <Application.Root>
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_SPOTIFY_CLIENT_ID: string;
+  readonly VITE_SPOTIFY_CLIENT_SECRET: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}

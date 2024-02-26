48ad3efc4e751a7b2a5baebd7fb7938df84c5206
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2024-02-26 13:19:18 -0300

    style: finish RecentProjects mobile style

--- src/components/RecentProjects.tsx
+++ src/components/RecentProjects.tsx
@@ -37,27 +37,44 @@ export function RecentProjects() {
       <Application.Header>My Recent Work</Application.Header>
 
       <Application.Content>
-        <ul className='list-none space-y-8 px-6'>
+        <ul className='list-none space-y-8 px-2 pt-1 md:px-6'>
           {projects.map(project => (
-            <li key={project.id}>
+            <li
+              key={project.id}
+              className='bg-white/10 rounded-2xl p-2 hover:scale-[1.03] transition-all duration-500'
+            >
               <a
+                target='_blank'
+                rel='noreferrer'
                 href={project.html_url}
-                className='flex items-center gap-1'
+                className='flex flex-col gap-3'
               >
-                <GithubIcon />
-                <h4 className='hover:underline text-lg font-semibold break-all'>
+                <h4 className='flex items-center gap-1 text-lg font-semibold break-all hover:underline'>
+                  <GithubIcon />
                   {project.name}
                 </h4>
+
+                <p className='text-ellipsis whitespace-nowrap overflow-hidden'>
+                  {project.description}
+                </p>
+
+                <div className='flex flex-wrap gap-2'>
+                  {project.topics && project.topics.length > 0 ? (
+                    project.topics.slice(0, 3).map(topic => (
+                      <span
+                        className='py-1 px-2 text-xs font-light break-all rounded-full bg-indigo-500 cursor-default'
+                        key={topic}
+                      >
+                        {topic}
+                      </span>
+                    ))
+                  ) : (
+                    <span className='py-1 px-2 text-xs font-light break-all rounded-full bg-indigo-500 cursor-default'>
+                      No topics found
+                    </span>
+                  )}
+                </div>
               </a>
-
-              <p className='text-ellipsis whitespace-nowrap overflow-hidden'>
-                {project.description}
-              </p>
-
-              <div>
-                {project.topics &&
-                  project.topics.map(topic => <p key={topic}>{topic}</p>)}
-              </div>
             </li>
           ))}
         </ul>

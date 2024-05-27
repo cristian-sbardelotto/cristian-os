e9b1c9eb2944840e5b42817790fd4660a9f473f2
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2024-05-27 14:04:06 -0300

    style: improve AboutMe reading experience

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -10,27 +10,29 @@ export function AboutMe() {
       <Application.Header>{t('apps.about-me')}</Application.Header>
 
       <Application.Content>
-        <p className='about-me-paragraph'>{t('about-me.paragraph-1')}</p>
+        <div className='max-w-5xl'>
+          <p className='about-me-paragraph'>{t('about-me.paragraph-1')}</p>
 
-        <p className='mt-4 about-me-paragraph'>{t('about-me.paragraph-2')}</p>
+          <p className='mt-4 about-me-paragraph'>{t('about-me.paragraph-2')}</p>
 
-        <p className='mt-4 about-me-paragraph'>{t('about-me.paragraph-3')}</p>
+          <p className='mt-4 about-me-paragraph'>{t('about-me.paragraph-3')}</p>
 
-        <p className='mt-4 about-me-paragraph'>{t('about-me.paragraph-4')}</p>
+          <p className='mt-4 about-me-paragraph'>{t('about-me.paragraph-4')}</p>
 
-        <p className='mt-4 about-me-paragraph'>{t('about-me.paragraph-5')}</p>
+          <p className='mt-4 about-me-paragraph'>{t('about-me.paragraph-5')}</p>
 
-        <p className='mt-4 about-me-paragraph'>
-          {t('about-me.paragraph-6')}{' '}
-          <a
-            className='underline hover:brightness-50 transition-all duration-500'
-            href='https://portfolio-chi-lemon-51.vercel.app'
-            target='_blank'
-            rel='noreferrer'
-          >
-            portfolio-chi-lemon-51.vercel.app
-          </a>
-        </p>
+          <p className='mt-4 about-me-paragraph'>
+            {t('about-me.paragraph-6')}{' '}
+            <a
+              className='underline hover:brightness-50 transition-all duration-500'
+              href='https://portfolio-chi-lemon-51.vercel.app'
+              target='_blank'
+              rel='noreferrer'
+            >
+              portfolio-chi-lemon-51.vercel.app
+            </a>
+          </p>
+        </div>
       </Application.Content>
     </Application.Root>
   );
